d35698af39eb9353f23a06c756871bf65d548d91
Thomas Talbot <thomas.talbot@ioni.tech>
2022-12-03 13:21:28 +0100

    add enhancements vendor + class spells + mounts

---
+++ TODO.md
@@ -0,0 +1,5 @@
+
+* Ajouter les Maitres de classe (.npc add)
+* P1 BiS / classe-role
+* Campagne DK
+
--- src/MyPlayer.cpp
+++ src/MyPlayer.cpp
@@ -37,6 +37,31 @@ public:
         player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(1119), 43000, false);
         player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(1106), 43000, false);
 
+        player->learnSpell(34091 /* Artisan cavalier */);
+        player->learnSpell(54197 /* Vol par temps froid */);
+        player->learnSpell(59976 /* Proto-drake noir */);
+        player->learnSpell(17481 /* Destrier de la mort de Vaillefendre */);
+
+        switch (player->getClass()) {
+            case CLASS_WARLOCK:
+                player->learnSpell(1122); // Inferno
+                player->learnSpell(691); // Felhunter
+                player->learnSpell(697); // Voidwalker
+                player->learnSpell(712); // Succubus
+                player->learnSpell(713); // Incubus
+                player->learnSpell(18540); // Ritual of Doom
+                break;
+            case CLASS_SHAMAN:
+                player->AddItem(5175, 1);
+                player->AddItem(5176, 1);
+                player->AddItem(5177, 1);
+                player->AddItem(5178, 1);
+                break;
+            case CLASS_PALADIN:
+                player->learnSpell(7328); // Redemption
+                break;
+        }
+
         player->TeleportTo(571, 5876.77, 659.164, 644.985, 1.22217); // Puit de Dalaran
     }
 };
