627f86742962906c71ca2862c68a8245843925f3
Thomas Talbot <thomas.talbot@ioni.tech>
2022-12-03 01:09:26 +0100

    add MyPlayer + Enchantor

---
+++ src/Enchantor.cpp
@@ -0,0 +1,133 @@
+#include "Player.h"
+#include "ScriptMgr.h"
+#include "ScriptedGossip.h"
+
+class Enchantor : public CreatureScript {
+public:
+    Enchantor() : CreatureScript("Enchantor") {}
+
+    bool OnGossipHello(Player *player, Creature *creature) {
+        AddGossipItemFor(player, 10, "Enchanteur 450 + recettes", GOSSIP_SENDER_MAIN, 1);
+        AddGossipItemFor(player, 10, "Couture 450 + recettes", GOSSIP_SENDER_MAIN, 2);
+        AddGossipItemFor(player, 10, "Joaillerie 450 + recettes", GOSSIP_SENDER_MAIN, 3);
+        AddGossipItemFor(player, 10, "Ingénierie 450 + recettes", GOSSIP_SENDER_MAIN, 4);
+        AddGossipItemFor(player, 10, "Alchimie 450 + recettes", GOSSIP_SENDER_MAIN, 5);
+        SendGossipMenuFor(player, 1, creature->GetGUID());
+
+        return true;
+    }
+
+    bool OnGossipSelect(Player *player, Creature * /*creature*/, uint32 /*sender*/, uint32 action) {
+        player->PlayerTalkClass->ClearMenus();
+
+        switch (action) {
+            case 1: // Enchant
+            {
+                CloseGossipMenuFor(player);
+
+                uint32 skillId = 333;
+
+                learnSkill(player, skillId);
+                player->SetSkill(skillId, 0, 450, 450);
+                if (!player->HasItemCount(44452)) {
+                    player->AddItem(44452, 1); // Bâtonnet runique en titane
+                }
+
+                break;
+            }
+            case 2: // Couture
+            {
+                CloseGossipMenuFor(player);
+
+                uint32 skillId = 197;
+
+                learnSkill(player, skillId);
+                player->SetSkill(skillId, 0, 450, 450);
+
+                break;
+            }
+            case 3: // Joaillerie
+            {
+                CloseGossipMenuFor(player);
+
+                uint32 skillId = 755;
+
+                learnSkill(player, skillId);
+                player->SetSkill(skillId, 0, 450, 450);
+                if (!player->HasItemCount(20824)) {
+                    player->AddItem(20824, 1); // Meule simple
+                }
+                if (!player->HasItemCount(20815)) {
+                    player->AddItem(20815, 1); // Trousse de joaillier
+                }
+
+                break;
+            }
+            case 4: // Ingénierie
+            {
+                CloseGossipMenuFor(player);
+
+                uint32 skillId = 202;
+
+                learnSkill(player, skillId);
+                player->SetSkill(skillId, 0, 450, 450);
+                if (!player->HasItemCount(40772)) {
+                    player->AddItem(40772, 1); // Couteau de l'armée gnome
+                }
+
+                break;
+            }
+            case 5: // Alchimie
+            {
+                CloseGossipMenuFor(player);
+
+                uint32 skillId = 171;
+
+                learnSkill(player, skillId);
+                player->SetSkill(skillId, 0, 450, 450);
+
+                break;
+            }
+        }
+
+        return true;
+    }
+
+private:
+    void learnSkill(Player *player, uint32 skillId) {
+        uint32 classmask = player->getClassMask();
+
+        for (uint32 j = 0; j < sSkillLineAbilityStore.GetNumRows(); ++j)
+        {
+            SkillLineAbilityEntry const* skillLine = sSkillLineAbilityStore.LookupEntry(j);
+            if (!skillLine)
+                continue;
+
+            // wrong skill
+            if (skillLine->SkillLine != skillId)
+                continue;
+
+            // not high rank
+            if (skillLine->SupercededBySpell)
+                continue;
+
+            // skip racial skills
+            if (skillLine->RaceMask != 0)
+                continue;
+
+            // skip wrong class skills
+            if (skillLine->ClassMask && (skillLine->ClassMask & classmask) == 0)
+                continue;
+
+            SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(skillLine->Spell);
+            if (!spellInfo || !SpellMgr::IsSpellValid(spellInfo))
+                continue;
+
+            player->learnSpell(skillLine->Spell);
+        }
+    }
+};
+
+void AddEnchantorScripts() {
+    new Enchantor();
+}
--- src/MyPlayer.cpp
+++ src/MyPlayer.cpp
@@ -1,9 +1,6 @@
-/*
- * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
- */
-
 #include "ScriptMgr.h"
 #include "Player.h"
+#include "ReputationMgr.h"
 #include "Config.h"
 #include "Chat.h"
 
@@ -15,10 +12,32 @@ public:
 
     void OnLogin(Player* player) override
     {
-        if (sConfigMgr->GetOption<bool>("MyModule.Enable", false))
-        {
-            ChatHandler(player->GetSession()).SendSysMessage("Hello World from Skeleton-Module!");
+        if (!sConfigMgr->GetOption<bool>("SPQR.Enable", false)) {
+            return;
         }
+        ChatHandler(player->GetSession()).SendSysMessage("SPQR module loaded.");
+    }
+
+    void OnFirstLogin(Player* player) override
+    {
+        if (!sConfigMgr->GetOption<bool>("SPQR.Enable", false)) {
+            return;
+        }
+
+        player->GiveLevel(DEFAULT_MAX_LEVEL);
+        player->ModifyMoney(2000000000);
+        for (uint16 slot = INVENTORY_SLOT_BAG_START; slot < INVENTORY_SLOT_BAG_END; ++slot) {
+            player->EquipNewItem(slot, 38082, true);
+        }
+        player->UpdateSkillsToMaxSkillsForLevel();
+
+        player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(1090), 43000, false);
+        player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(1098), 43000, false);
+        player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(1091), 43000, false);
+        player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(1119), 43000, false);
+        player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(1106), 43000, false);
+
+        player->TeleportTo(571, 5876.77, 659.164, 644.985, 1.22217); // Puit de Dalaran
     }
 };
 
